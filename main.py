e5f01937ab157b16192af8bb0f495004b75abf46
gkutyi <75100550+gkutyi@users.noreply.github.com>
2024-05-13 15:02:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ from WIFI_CONFIG import SSID, PASSWORD
 
 firmware_url = "https://raw.githubusercontent.com/gkutyi/Berger-LiFePo4-BLE2MQTT/main"
 
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+ota_updater.download_and_install_update_if_available()
+
 print ('hello world')
