9a6786e9768113881e66d78c64a3c4da72bdfd43
gkutyi <75100550+gkutyi@users.noreply.github.com>
2024-05-13 14:56:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+SSID = "WLAN-286-Modem"
+PASSWORD = "Hagenweg@286"
 
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+
+firmware_url = "https://raw.githubusercontent.com/gkutyi/Berger-LiFePo4-BLE2MQTT/main"
+
+print ('hello world')
