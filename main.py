477b6026f3398fa07fe900f1dd4adeab09689793
gkutyi <75100550+gkutyi@users.noreply.github.com>
2024-06-14 22:17:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+# This example finds and connects to a BLE temperature sensor (e.g. the one in ble_temperature.py).
+
+# This example demonstrates the low-level bluetooth module. For most
+# applications, we recommend using the higher-level aioble library which takes
+# care of all IRQ handling and connection management. See
+# https://github.com/micropython/micropython-lib/tree/master/micropython/bluetooth/aioble
+# and in particular the temp_client.py example included with aioble.
+
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD, SSID_TEST, PASSWORD_TEST
-from BROKER import MQTT_BROKER, MQTT_PORT, MQTT_USER, MQTT_PW, MQTT_TOPIC, MQTT_OTA_UPDATE, MQTT_SSL
-from umqtt.simple import MQTTClient
 import ubluetooth as bluetooth
 import time
 import network
@@ -9,151 +15,216 @@ import machine
 import urequests
 import ussl
 import socket
+import random
+import struct
+import micropython
 
-# SSL/TLS Parameters
-root_ca = "/ssl/fullchain.pem"  # Path to the root CA certificate
+from ble_advertising import decode_services, decode_name
 
-# Load the CA certificate
-with open(root_ca, "r") as f:
-    ca_cert = f.read()
+from micropython import const
 
-# Define the UUID of the characteristic
-CHARACTERISTIC_UUID = '00001101-0000-1000-8000-00805F9B34FB'
+_IRQ_CENTRAL_CONNECT = const(1)
+_IRQ_CENTRAL_DISCONNECT = const(2)
+_IRQ_GATTS_WRITE = const(3)
+_IRQ_GATTS_READ_REQUEST = const(4)
+_IRQ_SCAN_RESULT = const(5)
+_IRQ_SCAN_DONE = const(6)
+_IRQ_PERIPHERAL_CONNECT = const(7)
+_IRQ_PERIPHERAL_DISCONNECT = const(8)
+_IRQ_GATTC_SERVICE_RESULT = const(9)
+_IRQ_GATTC_SERVICE_DONE = const(10)
+_IRQ_GATTC_CHARACTERISTIC_RESULT = const(11)
+_IRQ_GATTC_CHARACTERISTIC_DONE = const(12)
+_IRQ_GATTC_DESCRIPTOR_RESULT = const(13)
+_IRQ_GATTC_DESCRIPTOR_DONE = const(14)
+_IRQ_GATTC_READ_RESULT = const(15)
+_IRQ_GATTC_READ_DONE = const(16)
+_IRQ_GATTC_WRITE_DONE = const(17)
+_IRQ_GATTC_NOTIFY = const(18)
+_IRQ_GATTC_INDICATE = const(19)
 
-# Create a BLE object
-ble = bluetooth.BLE()
+_ADV_IND = const(0x00)
+_ADV_DIRECT_IND = const(0x01)
+_ADV_SCAN_IND = const(0x02)
+_ADV_NONCONN_IND = const(0x03)
 
-# MAC-Adresse des BLE-Geräts
-ble_address = 'XX:XX:XX:XX:XX:XX'
+# org.bluetooth.service.environmental_sensing
+_ENV_SENSE_UUID = bluetooth.UUID(0x181A)
+# org.bluetooth.characteristic.temperature
+_TEMP_UUID = bluetooth.UUID(0x2A6E)
 
-# MQTT-Verbindungsdetails
-mqtt_broker = MQTT_BROKER
-mqtt_port = MQTT_PORT
-mqtt_user = MQTT_USER
-mqtt_password = MQTT_PW
-mqtt_topic = MQTT_TOPIC
-ota_topic = MQTT_OTA_UPDATE
-mqtt_ssl = MQTT_SSL
 
-# Wi-Fi-Verbindungsdetails
-wifi_ssid = SSID
-wifi_password = PASSWORD
-wifi_ssid_test = SSID_TEST
-wifi_password_test = PASSWORD_TEST
+# org.bluetooth.characteristic.gap.appearance.xml
+_ADV_APPEARANCE_GENERIC_THERMOMETER = const(768)
 
-# MQTT-Client-Instanz erstellen
-mqtt_client = None
 
-# Callback-Funktion für das BLE-Scanergebnis
-def scan_callback(event, data):
-    print('BLE-Scan')
-    print("Event:", event, "with data:", data)
-    if event == 1: # EVT_GAP_SCAN_RESULT
-        # Parse the data to extract information about the scanned device
-        _, addr_type, addr, _, _, adv_data = data
-        print("Found device with address:", addr)
-        print("Address type:", addr_type)
-        print("Advertisement data:", adv_data)
-    if event == 5: # EVT_GAP_SCAN_RESULT
-        # Parse the data to extract information about the scanned device
-        addr_type, addr, adv_type, rssi, adv_data = data
-        print("Found device with address:", bytes(addr))
-        print("Address type:", addr_type)
-        print("Advertisement data:", bytes(adv_data))
-        print("RSSI:", rssi)
-        print("Advertisment type:", adv_type)
-    if event == 3: # EVENT_ADV_IND
-        addr_type, addr, adv_type, rssi, adv_data = data
-        if addr == ble_address:
-            # Verbindung herstellen
-            ble_connection = bluetooth.BLE()
-            ble_connection.active(True)
-            peripheral = ble_connection.connect(addr)
+class BLETemperatureCentral:
+    def __init__(self, ble):
+        self._ble = ble
+        self._ble.active(True)
+        self._ble.irq(self._irq)
 
-            # Dienst und Charakteristik für Nachrichtenlesen
-            services = peripheral.services()
-            for service in services:
-                characteristics = service.characteristics()
-                for char in characteristics:
-                    if char.uuid() == 'UUID_der_Charakteristik':
-                        while True:
-                            value = char.read()
-                            # Hier können Sie die empfangenen Werte verarbeiten
-                            print('Received value:', value)
-                            # Sende Wert über MQTT
-                            publish_to_mqtt(value)
+        self._reset()
 
-# Callback-Funktion für eingehende MQTT-Nachrichten
-def mqtt_callback(topic, msg):
-    print("Received message on topic:", topic.decode(), "with message:", msg.decode())
-    if msg.decode() == 'now' and topic.decode() == ota_topic:
-    # Nachricht zum Starten des OTA-Updates empfangen
-        print('OTA update message received.', msg.decode)
-        # Hier können Sie das OTA-Update durchführen
-        if perform_ota_update():
-            # Sende Wert über MQTT
-            publish_to_mqtt(ota_topic, 'success')
-        else: publish_to_mqtt(ota_topic, 'failure')    
-    
-# Verbindung zu MQTT-Broker herstellen
-def connect_mqtt():
-    # Create an SSL context
-    ssl_params = {
-        'server_hostname': mqtt_broker,
-        'certfile': None,
-        'keyfile': None,
-        'cert_reqs': ussl.CERT_REQUIRED,
-        'cadata': ca_cert,
-    }
-    global mqtt_client
-    mqtt_client = MQTTClient('esp32', mqtt_broker, port=mqtt_port, user=mqtt_user, password=mqtt_password, ssl=mqtt_ssl, ssl_params=ssl_params)
-    mqtt_client.set_callback(mqtt_callback)  # Set the callback function
-    try:
-        mqtt_client.connect()
-        print('Connected to MQTT-Broker')
+    def _reset(self):
+        # Cached name and address from a successful scan.
+        self._name = None
+        self._addr_type = None
+        self._addr = None
+
+        # Cached value (if we have one)
+        self._value = None
+
+        # Callbacks for completion of various operations.
+        # These reset back to None after being invoked.
+        self._scan_callback = None
+        self._conn_callback = None
+        self._read_callback = None
+
+        # Persistent callback for when new data is notified from the device.
+        self._notify_callback = None
+
+        # Connected device.
+        self._conn_handle = None
+        self._start_handle = None
+        self._end_handle = None
+        self._value_handle = None
+
+    def _irq(self, event, data):
+        if event == _IRQ_SCAN_RESULT:
+            addr_type, addr, adv_type, rssi, adv_data = data
+            if adv_type in (_ADV_IND, _ADV_DIRECT_IND) and _ENV_SENSE_UUID in decode_services(
+                adv_data
+            ):
+                # Found a potential device, remember it and stop scanning.
+                self._addr_type = addr_type
+                self._addr = bytes(
+                    addr
+                )  # Note: addr buffer is owned by caller so need to copy it.
+                self._name = decode_name(adv_data) or "?"
+                self._ble.gap_scan(None)
+
+        elif event == _IRQ_SCAN_DONE:
+            if self._scan_callback:
+                if self._addr:
+                    # Found a device during the scan (and the scan was explicitly stopped).
+                    self._scan_callback(self._addr_type, self._addr, self._name)
+                    self._scan_callback = None
+                else:
+                    # Scan timed out.
+                    self._scan_callback(None, None, None)
+
+        elif event == _IRQ_PERIPHERAL_CONNECT:
+            # Connect successful.
+            conn_handle, addr_type, addr = data
+            if addr_type == self._addr_type and addr == self._addr:
+                self._conn_handle = conn_handle
+                self._ble.gattc_discover_services(self._conn_handle)
+
+        elif event == _IRQ_PERIPHERAL_DISCONNECT:
+            # Disconnect (either initiated by us or the remote end).
+            conn_handle, _, _ = data
+            if conn_handle == self._conn_handle:
+                # If it was initiated by us, it'll already be reset.
+                self._reset()
+
+        elif event == _IRQ_GATTC_SERVICE_RESULT:
+            # Connected device returned a service.
+            conn_handle, start_handle, end_handle, uuid = data
+            if conn_handle == self._conn_handle and uuid == _ENV_SENSE_UUID:
+                self._start_handle, self._end_handle = start_handle, end_handle
+
+        elif event == _IRQ_GATTC_SERVICE_DONE:
+            # Service query complete.
+            if self._start_handle and self._end_handle:
+                self._ble.gattc_discover_characteristics(
+                    self._conn_handle, self._start_handle, self._end_handle
+                )
+            else:
+                print("Failed to find environmental sensing service.")
+
+        elif event == _IRQ_GATTC_CHARACTERISTIC_RESULT:
+            # Connected device returned a characteristic.
+            conn_handle, def_handle, value_handle, properties, uuid = data
+            if conn_handle == self._conn_handle and uuid == _TEMP_UUID:
+                self._value_handle = value_handle
+
+        elif event == _IRQ_GATTC_CHARACTERISTIC_DONE:
+            # Characteristic query complete.
+            if self._value_handle:
+                # We've finished connecting and discovering device, fire the connect callback.
+                if self._conn_callback:
+                    self._conn_callback()
+            else:
+                print("Failed to find temperature characteristic.")
+
+        elif event == _IRQ_GATTC_READ_RESULT:
+            # A read completed successfully.
+            conn_handle, value_handle, char_data = data
+            if conn_handle == self._conn_handle and value_handle == self._value_handle:
+                self._update_value(char_data)
+                if self._read_callback:
+                    self._read_callback(self._value)
+                    self._read_callback = None
+
+        elif event == _IRQ_GATTC_READ_DONE:
+            # Read completed (no-op).
+            conn_handle, value_handle, status = data
+
+        elif event == _IRQ_GATTC_NOTIFY:
+            # The ble_temperature.py demo periodically notifies its value.
+            conn_handle, value_handle, notify_data = data
+            if conn_handle == self._conn_handle and value_handle == self._value_handle:
+                self._update_value(notify_data)
+                if self._notify_callback:
+                    self._notify_callback(self._value)
+
+    # Returns true if we've successfully connected and discovered characteristics.
+    def is_connected(self):
+        return self._conn_handle is not None and self._value_handle is not None
+
+    # Find a device advertising the environmental sensor service.
+    def scan(self, callback=None):
+        self._addr_type = None
+        self._addr = None
+        self._scan_callback = callback
+        self._ble.gap_scan(2000, 30000, 30000)
+
+    # Connect to the specified device (otherwise use cached address from a scan).
+    def connect(self, addr_type=None, addr=None, callback=None):
+        self._addr_type = addr_type or self._addr_type
+        self._addr = addr or self._addr
+        self._conn_callback = callback
+        if self._addr_type is None or self._addr is None:
+            return False
+        self._ble.gap_connect(self._addr_type, self._addr)
         return True
-    except Exception as e:
-        print(f"Failed to connect to MQTT broker: {e}")
-        return False
 
-# Nachricht über MQTT veröffentlichen
-def publish_to_mqtt(topic, value):
-    global mqtt_client
-    mqtt_client.publish(topic, str(value))
+    # Disconnect from current device.
+    def disconnect(self):
+        if self._conn_handle is None:
+            return
+        self._ble.gap_disconnect(self._conn_handle)
+        self._reset()
 
-# Auf MQTT-Nachrichten prüfen
-def check_mqtt_messages():
-    # Über MQTT prüfen, ob ein OTA-Update erforderlich ist
-    mqtt_client.subscribe(ota_topic)
-    while True:
-        try:
-            mqtt_client.check_msg()  # Check for incoming message
-        except Exception as e:
-            print(f"Error checking messages: {e}")
-            reconnect_mqtt()
+    # Issues an (asynchronous) read, will invoke callback with data.
+    def read(self, callback):
+        if not self.is_connected():
+            return
+        self._read_callback = callback
+        self._ble.gattc_read(self._conn_handle, self._value_handle)
 
-def reconnect_mqtt():
-    connected = False
-    while not connected:
-        connected = connect_mqtt()
-        if not connected:
-            print("Retrying MQTT connection in 5 seconds...")
-            time.sleep(5)  # Wait a short time
+    # Sets a callback to be invoked when the device notifies us.
+    def on_notify(self, callback):
+        self._notify_callback = callback
 
-# OTA-Update durchführen
-def perform_ota_update():
-    firmware_url = "https://raw.githubusercontent.com/gkutyi/Berger-LiFePo4-BLE2MQTT/"
-    ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
-    if ota_updater.download_and_install_update_if_available():
-        return True
-    else: return False
+    def _update_value(self, data):
+        # Data is sint16 in degrees Celsius with a resolution of 0.01 degrees Celsius.
+        self._value = struct.unpack("<h", data)[0] / 100
+        return self._value
 
-# BLE-Scan starten
-def start_ble_scan():
-    ble.active(True)
-    ble.gap_scan(0)  # Start scanning, 0 means continuous scanning
-    ble.irq(scan_callback) # Set the scan callback
-#    ble.gap_scan(1)  # Enable scanning     
+    def value(self):
+        return self._value
 
 # Function to reset the WiFi interface
 def reset_wifi_interface():
@@ -195,18 +266,56 @@ def connect_to_wifi(ssid, password):
     print(f'Failed to connect to {ssid} after {attempts} attempts')
     return False
 
+# OTA-Update durchführen
+def perform_ota_update():
+    firmware_url = "https://raw.githubusercontent.com/gkutyi/Berger-LiFePo4-BLE2MQTT/"
+    ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+    if ota_updater.download_and_install_update_if_available():
+        return True
+    else: return False
 
-# Hauptfunktion
-def main():
+def demo():
+    ble = bluetooth.BLE()
+    central = BLETemperatureCentral(ble)
+
+    not_found = False
+
+    def on_scan(addr_type, addr, name):
+        if addr_type is not None:
+            print("Found sensor:", addr_type, addr, name)
+            central.connect()
+        else:
+            nonlocal not_found
+            not_found = True
+            print("No sensor found.")
+
     # Try to connect to the primary WiFi network
     if not connect_to_wifi(wifi_ssid, wifi_password):
         # If the primary connection fails, try the secondary WiFi network
-        connect_to_wifi(wifi_ssid_test, wifi_password_test)
+        connect_to_wifi(wifi_ssid_test, wifi_password_test)    
     perform_ota_update()
-#. Connect_mqtt()
-    start_ble_scan()
-#. check_mqtt_messages()
+    central.scan(callback=on_scan)
+
+    # Wait for connection...
+    while not central.is_connected():
+        time.sleep_ms(100)
+        if not_found:
+            return
+
+    print("Connected")
+
+    # Explicitly issue reads, using "print" as the callback.
+    while central.is_connected():
+        central.read(callback=print)
+        time.sleep_ms(2000)
+
+    # Alternative to the above, just show the most recently notified value.
+    # while central.is_connected():
+    #     print(central.value())
+    #     time.sleep_ms(2000)
+
+    print("Disconnected")
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    demo()
