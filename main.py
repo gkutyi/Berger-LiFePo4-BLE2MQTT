fc5f3a9619894d9639e7b0ad73cb6e9d281f8735
gkutyi <75100550+gkutyi@users.noreply.github.com>
2024-06-16 22:25:05 +0200

    main.py aktualisieren

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ async def find_temp_sensor():
     # maximise detection rate).
     async with central.scan(5000, interval_us=30000, window_us=30000, active=True) as scanner:
         async for result in scanner:
-            print(result, result.name(), result.rssi, result.services())
+            print(result, result.name(), result.rssi, bytes(result.services()))
             # See if it matches our name and the environmental sensing service.
             if result.name() == "BT-Battery" and _BTBATT_UUID in result.services():
                 return result.device
